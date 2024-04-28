38244a792591f9298e0374f5a6a3c0cf3633fe66
Sergio Cervantes <sergio_germanc@hotmail.com>
2024-04-28 09:12:07 -0600

    Add hello.py and reverseCypher.py

---
+++ hello.py
@@ -0,0 +1,6 @@
+spam = 'Hello ' * 3
+print(spam)
+
+if spam == 'Hello Hello Hello ':
+    for letter in spam:
+        print(letter)
---
+++ reverseCypher.py
@@ -0,0 +1,11 @@
+message = 'olleh'
+
+translated = ''
+
+i = len(message) - 1
+
+while i >= 0:
+    translated = translated + message[i]
+    i = i - 1
+
+print(translated)
